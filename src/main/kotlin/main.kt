6e9c9141dddb678685ad7906166406eaa97142ab
Sergey <sergey@localhost>
2021-10-26 21:46:24 +0300

    Add Objects

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 fun main() {
 
-    val original = Post()
+    val original = Post(id = 1)
 
     WallService.add(original)
 }
@@ -16,68 +16,68 @@ object WallService {
 
 data class Post(
     val id: Int,
-    val ownerId: Int,
-    val fromId: Int,
-    val createdBy: Int,
-    val date: Int,
-    val text: Int,
-    val replyOwnerId: Int,
-    val replyPostId: Int,
-    val friendsOnly: Boolean,
-    val comments: Comments,
-    val copyright: Copyright,
-    val likes: Likes,
-    val reposts: Reposts,
-    val views: Views,
-    val postType: String,
-    val signerId: Int,
-    val canPin: Boolean,
-    val canDelete: Boolean,
-    val canEdit: Boolean,
-    val isPinned: Boolean,
-    val markedAsAds: Boolean,
+    val ownerId: Int = 0,
+    val fromId: Int = 0,
+    val createdBy: Int = 0,
+    val date: Int = 0,
+    val text: Int = 0,
+    val replyOwnerId: Int = 0,
+    val replyPostId: Int = 0,
+    val friendsOnly: Boolean = true,
+    val comments: Comments = Comments(),
+    val copyright: Copyright = Copyright(),
+    val likes: Likes = Likes(),
+    val reposts: Reposts = Reposts(),
+    val views: Views = Views(),
+    val postType: String = "",
+    val signerId: Int = 0,
+    val canPin: Boolean = true,
+    val canDelete: Boolean = true,
+    val canEdit: Boolean = true,
+    val isPinned: Boolean = true,
+    val markedAsAds: Boolean = true,
     val isFavorite: Boolean = true,
-    val donut: Donat,
-    val postponedId: Int
+    val donut: Donat = Donat(),
+    val postponedId: Int = 0
 )
 
 class Comments(
-    val count: Int,
-    val canPost: Boolean,
-    val groupsCanPost: Boolean,
-    val canClose: Boolean,
-    val canOpen: Boolean
+    val count: Int = 0,
+    val canPost: Boolean = true,
+    val groupsCanPost: Boolean = true,
+    val canClose: Boolean = true,
+    val canOpen: Boolean = true
 )
 
 class Copyright(
-    val id: Int,
-    val link: String,
-    val name: String,
-    val type: String
+    val id: Int = 0,
+    val link: String = " ",
+    val name: String = " ",
+    val type: String = " "
 )
 
 class Likes(
-    val count: Int,
-    val userLikes: Boolean,
-    val canLike: Boolean,
-    val canPublish: Boolean
+    val count: Int = 0,
+    val userLikes: Boolean = true,
+    val canLike: Boolean = true,
+    val canPublish: Boolean = true
 )
 
 class Reposts(
-    val count: Int,
-    val userReposted: Boolean
+    val count: Int = 0,
+    val userReposted: Boolean = true
 )
 
 class Views(
-    val count: Int
+    val count: Int = 0
 )
 
 class Donat(
-    val is_donut: Boolean,
-    val paidDuration: Int,
-    val placeholder: String,
-    val canPublishFreeCopy: Boolean,
-    val edit_mode: String,
-    val all: String,
-    val duration: Int
+    val is_donut: Boolean = true,
+    val paidDuration: Int = 0,
+    val placeholder: String = " ",
+    val canPublishFreeCopy: Boolean = true,
+    val edit_mode: String = " ",
+    val all: String = " ",
+    val duration: Int = 0
 )
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,18 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @Test
+    fun add() {
+        //arrange
+
+
+        //act
+        val result = 1
+
+        //assert
+        assertEquals(1, result)
+    }
+}
